093d77c80248cbbcd9b8d99a9a54fc8c2c99a26e
mrideout1985 <mrideout1985@gmail.com>
2024-03-16 15:27:59 +0000

    removal of unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,7 @@ import '~/styles/global.scss'
 import { ThemeProvider } from '@mui/material/styles'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
 import type { ReactElement, ReactNode } from 'react'
-import { lazy } from 'react'
 
 import Layout from '~/layouts/layout/Layout'
 import { theme } from '~/styles/theme'
@@ -22,27 +20,6 @@ export interface SharedPageProps {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
-
-const PreviewProvider = lazy(() => import('~/components/PreviewProvider'))
-
-const mono = IBM_Plex_Mono({
-  variable: '--font-family-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
-const sans = Inter({
-  variable: '--font-family-sans',
-  subsets: ['latin'],
-  weight: ['500', '700', '800'],
-})
-
-const serif = PT_Serif({
-  variable: '--font-family-serif',
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)

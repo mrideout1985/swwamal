d4d2bf4796439322f10f48ba1545e2c4bf9db04f
mrideout1985 <mrideout1985@gmail.com>
2024-03-16 16:02:40 +0000

    navbar for mobile

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -5,8 +5,51 @@ import { useMedia } from 'react-use'
 
 import CWU from '../icons/cwu-logo.svg'
 
-const MobileNav = () => {
-  return <div>MobileNav</div>
+const MobileNav = ({ pages }: { pages: string[] }) => {
+  const router = useRouter()
+
+  return (
+    <AppBar position="static">
+      <Container>
+        <Toolbar
+          variant="regular"
+          disableGutters
+          component="nav"
+          aria-label="primary navigation"
+          sx={{
+            width: '100%',
+          }}
+        >
+          <Box>
+            {pages.map((page) => (
+              <Link href={`${page === 'home' ? '/' : `/${page}`}`} key={page}>
+                <Button
+                  disableRipple
+                  sx={{
+                    borderRadius: '0',
+                    fontFamily: "Montserrat, 'sans-serif'",
+                    fontSize: '1rem',
+                    fontWeight: 600,
+                    '&:hover': {
+                      backgroundColor: 'transparent',
+                    },
+                    color:
+                      router.pathname === (page === 'home' ? '/' : `/${page}`)
+                        ? '#ff0077'
+                        : 'none',
+                  }}
+                  variant="text"
+                  color="secondary"
+                >
+                  {page}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
 }
 
 const DesktopNav = ({ pages }: { pages: string[] }) => {
@@ -65,7 +108,9 @@ const Navigation = () => {
   const isWide = useMedia('(min-width: 600px)', false)
   const pages = ['home', 'about', 'reps', 'agreements']
 
-  return <>{isWide ? <DesktopNav pages={pages} /> : <MobileNav />}</>
+  return (
+    <>{isWide ? <DesktopNav pages={pages} /> : <MobileNav pages={pages} />}</>
+  )
 }
 
 export default Navigation
